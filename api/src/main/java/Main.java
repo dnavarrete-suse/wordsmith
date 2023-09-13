db6356e986a0f4c659f252bb4a1fae8df290d9f3
dnavarrete-suse <david.navarrete@suse.com>
2023-09-13 14:55:32 +0200

    Prometheus Counters added

--- api/src/main/java/Main.java
+++ api/src/main/java/Main.java
@@ -3,14 +3,26 @@ import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.sql.*;
 import java.util.NoSuchElementException;
 
+//Prometheus imports -- monitoring-demo
+import io.prometheus.client.Counter;
+import io.prometheus.client.exporter.HTTPServer;
+
 public class Main {
+
+
+    //Prometheus metric builder -- monitoring-demo
+    static final Counter db_request_counter = Counter.build().name("db_requests_total").help("Calls to DB counter").register();
+    static final Counter db_failure_counter = Counter.build().name("db_requests_failed_total").help("Failed calls to DB counter").register();
+
+
     public static void main(String[] args) throws Exception {
+	
+
         Class.forName("org.postgresql.Driver");
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
@@ -21,15 +33,23 @@ public class Main {
     }
 
     private static String randomWord(String table) {
+
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://db:5432/postgres", "postgres", "")) {
             try (Statement statement = connection.createStatement()) {
                 try (ResultSet set = statement.executeQuery("SELECT word FROM " + table + " ORDER BY random() LIMIT 1")) {
+
+                    //Prometheus counter increment when the connection to the db is made-- monitoring-demo
+                    db_request_counter.inc();
+
                     while (set.next()) {
                         return set.getString(1);
                     }
                 }
             }
         } catch (SQLException e) {
+            //Prometheus counter increment when the request to the db fails -- monitoring-demo
+            db_failure_counter.inc();
+
             e.printStackTrace();
         }
 
